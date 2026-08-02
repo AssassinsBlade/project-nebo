1ea7e76ded2c0b405f15f28b06616b53c350fbe2
AssassinsBlade <38591176+AssassinsBlade@users.noreply.github.com>
2026-08-02 11:10:55 -0700

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,37 +1,24 @@
 function initializeTheme() {
-
     const themeButton = document.getElementById("themeToggle");
-
     const savedTheme = localStorage.getItem("nebo-theme");
-
     if (savedTheme) {
         document.documentElement.setAttribute("data-theme", savedTheme);
     }
-
-
     themeButton.addEventListener("click", () => {
-
+        console.log("button clicked");
         const currentTheme =
             document.documentElement.getAttribute("data-theme");
-
-
         const newTheme =
             currentTheme === "light"
                 ? "dark"
                 : "light";
-
-
         document.documentElement.setAttribute(
             "data-theme",
             newTheme
         );
-
-
         localStorage.setItem(
             "nebo-theme",
             newTheme
         );
-
     });
-
 }
