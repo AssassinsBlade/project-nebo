78d2a2c013998b17620d391ecd53cd8d5b9596cd
AssassinsBlade <38591176+AssassinsBlade@users.noreply.github.com>
2026-08-02 09:37:22 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-console.log("Project Nebo initialized.");
+function initializeTheme() {
+
+    const themeButton = document.getElementById("themeToggle");
+
+    const savedTheme = localStorage.getItem("nebo-theme");
+
+    if (savedTheme) {
+        document.documentElement.setAttribute("data-theme", savedTheme);
+    }
+
+
+    themeButton.addEventListener("click", () => {
+
+        const currentTheme =
+            document.documentElement.getAttribute("data-theme");
+
+
+        const newTheme =
+            currentTheme === "light"
+                ? "dark"
+                : "light";
+
+
+        document.documentElement.setAttribute(
+            "data-theme",
+            newTheme
+        );
+
+
+        localStorage.setItem(
+            "nebo-theme",
+            newTheme
+        );
+
+    });
+
+}
